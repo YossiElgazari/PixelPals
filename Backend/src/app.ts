8e90ed335f7280a6a702e1d5ab155b975140b3d5
Yossi Elgazari <yossielg98@gmail.com>
2024-04-08 01:26:33 +0300

    Initialized frontend

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,6 +1,7 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import connectDB from './config/db'; 
 import userRoutes from './routes/userRoutes'; 
+import postRoutes from './routes/postRoutes';
 
 connectDB();
 
@@ -11,9 +12,7 @@ app.use(express.json());
 
 app.use('/users', userRoutes);
 
-app.get('/', (req: Request, res: Response): void => {
-    res.send('Hello, PixelPals!');
-});
+app.use('/post', postRoutes)
 
 app.listen(port, (): void => {
   console.log(`Server is running on port ${port}`);
